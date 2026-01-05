4eaf4a1f1a8e1a74b257db2aa4ddba9144c44d86
anisulislampranto <76773444+anisulislampranto@users.noreply.github.com>
2026-01-05 17:03:00 +0600

    create user with extended fields

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -7,6 +7,27 @@ export const auth = betterAuth({
     database: prismaAdapter(prisma, {
         provider: "postgresql",
     }),
+    trustedOrigins: [
+        process.env.APP_URL!
+    ],
+    user: {
+        additionalFields: {
+            role: {
+                type: 'string',
+                defaultValue: 'user',
+                required: false
+            },
+            phone: {
+                type: 'string',
+                required: false
+            },
+            status: {
+                type: 'string',
+                defaultValue: "ACTIVE",
+                required: false
+            }
+        }
+    },
     emailAndPassword: {
         enabled: true,
     },
