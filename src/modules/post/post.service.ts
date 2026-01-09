d9b152b3aaa1fc4a01ad81de6ff49fd588d35cb1
anisulislampranto <76773444+anisulislampranto@users.noreply.github.com>
2026-01-09 21:33:50 +0600

    my posts count using aggregate and count

--- src/modules/post/post.service.ts
+++ src/modules/post/post.service.ts
@@ -184,10 +184,35 @@ const getMyPosts = async(authorId: string) => {
         },
         orderBy: {
             createdAt: "desc"
+        },
+        include: {
+            _count: {
+                select: {
+                    comments: true
+                }
+            }
         }
     })
 
-    return result;
+    // const total = await prisma.post.count({
+    //     where: {
+    //         authorId
+    //     },
+    // })
+
+    const total = await prisma.post.aggregate({
+        _count: {
+            id: true
+        },
+        where: {
+            authorId
+        },
+    })
+
+    return {
+        data: result,
+        total
+    };
 }
 
 export const postService = {
