9f4f5ad2fd8db3c62c79625cc9e21889d918166c
anisulislampranto <76773444+anisulislampranto@users.noreply.github.com>
2026-01-10 17:25:45 +0600

    PrismaClientValidationError

--- src/middlewere/globalErrorHandler.ts
+++ src/middlewere/globalErrorHandler.ts
@@ -1,9 +1,20 @@
+import { PrismaClientValidationError } from "@prisma/client/runtime/client";
 import { NextFunction, Request, Response } from "express"
 
 export default function errorHandler(err: any, req: Request, res: Response, next: NextFunction) {
-    res.status(500)
+    let statusCode = 500;
+    let errorMessage = 'Internal server Error'
+    let errorDetails = err
+
+    // prisma client validation error
+    if (err instanceof PrismaClientValidationError) {
+        statusCode = 400;
+        errorMessage = 'You have provided incorrect field type or missing fields!';
+    }
+
+    res.status(statusCode)
     res.json({
-        message: "Error from errorHandler",
-        error: err
+        message: errorMessage,
+        error: errorDetails
     })
 }
